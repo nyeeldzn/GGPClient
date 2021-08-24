bf1db43b7fdb958375fb517f23b28faa4781a4ca
Daniel Soares <nyeeldzn@gmail.com>
2021-08-23 22:37:26 -0300

    + PedidoService

---
+++ src/main/java/Services/PedidoService.java
@@ -0,0 +1,65 @@
+package Services;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import helpers.HTTPRequest.DefaultRequests;
+import models.OrdemPedido;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+public class PedidoService {
+
+        public static OrdemPedido getById(Long id){
+            OrdemPedido ped = new OrdemPedido();
+            try {
+                String json = DefaultRequests.getObject(id, "/clientes");
+
+                Gson gson = new Gson();
+                ped = gson.fromJson(new String(json.getBytes()), OrdemPedido.class);
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+            return ped;
+        }
+
+        public static ArrayList<OrdemPedido> findAll(){
+            ArrayList<OrdemPedido> pedidos = new ArrayList<>();
+            String json = DefaultRequests.getAll("/'pedidos'");
+
+            Gson gson = new Gson();
+
+            Type userListType = new TypeToken<ArrayList<OrdemPedido>>(){}.getType();
+
+            pedidos = gson.fromJson(json, userListType);
+
+            for(int i = 0; i<pedidos.size(); i++){
+                System.out.println("Pedido de id: " + pedidos.get(i).getId() + " recuperado." );
+            }
+
+            return pedidos;
+        }
+
+        public static String insert(OrdemPedido pedido){
+            Gson gson = new Gson();
+            String input = gson.toJson(pedido);
+            System.out.println("Entrada do POST" + input);
+            String output = DefaultRequests.postObject("/pedidos", input);
+            System.out.println("Saida do POST" + output);
+            return output;
+        }
+
+        public static int update(OrdemPedido newObj){
+            Gson gson = new Gson();
+            String json = gson.toJson(newObj);
+
+            return DefaultRequests.putObject("/pedidos", json);
+        }
+
+        public static int delete(Long id){
+            int state;
+                state = DefaultRequests.deleteObject(id, "/pedidos");
+            return state;
+        }
+    }
